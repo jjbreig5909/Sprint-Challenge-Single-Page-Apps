4b77320c2469ffb48b6d122783d4222b40ff1ffa
Jeff Breig <jjbreig@gmail.com>
2019-12-15 22:25:56 -0500

    added navigation to app and index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage"
+import styled from "styled-components";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/characters">Characters</Link>
+      </nav>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
