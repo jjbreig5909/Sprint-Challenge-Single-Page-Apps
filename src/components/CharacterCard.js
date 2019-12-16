21ae9ec3eff5128a4b8ead9085cc7070aa1c7e2b
Jeff Breig <jjbreig@gmail.com>
2019-12-15 22:14:11 -0500

    added character list, search, and cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 20px;
+margin: 0 auto 20px auto;
+background: lightblue;
+border-radius: 10px;
+width: 50%;
+`
+
+const CardContent = styled.div`
+display: flex;
+flex-direction: column;
+margin-left: 20px;
+
+`
+
+const CardImage = styled.img`
+margin=50px; 
+`
+
+
+export default function CharacterCard(props) {
+  return (
+
+  <Card>
+    <CardImage src={props.image} alt={`${props.name}`}></CardImage>
+    <CardContent>
+      <h3>{props.name}</h3>
+      <p>Gender: {props.gender}</p>
+      <p>Location: {props.location}</p>
+    </CardContent>
+
+  </Card>
+
+  )  
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SearchForm from "./SearchForm"
+import styled from "styled-components";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  
+
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setCharacters(response.data.results);
+        console.log(response);
+      })
+      .catch(error=> {
+        console.log("Couldn't get any data!", error);
+      });
+
   }, []);
+
+
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>The Cast of Rick and Morty</h2>
+      <SearchForm characters={characters}/>
+      
     </section>
   );
 }
